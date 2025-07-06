ca7deba9dcdd211358881b15935547974ad98a95
Jonas <92853498+joraka@users.noreply.github.com>
2025-07-06 23:44:57 +0300

    cypress gh action

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,3 @@
-/// <reference types="cypress" />
-/// <reference types="../support/commands.d.ts" />
-
 beforeEach(() => {
   cy.visit("http://localhost:5173/");
 });
@@ -8,17 +5,13 @@ beforeEach(() => {
 describe("Test case 1: Grid and buttons are displayed", () => {
   it("should display grid and go to start button", () => {
     cy.contains(".status", "Next player: X").should("be.visible");
-
-    cy.get(".board-row button")
+    cy.getGameTileList()
       .should("have.length", 9)
       .each(($button) => {
         cy.wrap($button).should("be.visible");
       });
 
-    cy.get(".game-info button")
-      .should("have.length", 1)
-      .should("be.visible")
-      .should("have.text", "Go to game start");
+    cy.get(".game-info button").should("have.length", 1).should("be.visible").should("have.text", "Go to game start");
   });
 });
 
@@ -39,14 +32,62 @@ describe("Test case 2: Place tiles", () => {
 });
 
 describe("Test case 3: Win conditions", () => {
+  it("shoud detect player X win", () => {
+    cy.getGameTileList().then(($tileList) => {
+      let index = 0;
+      cy.wrap($tileList).eq(index).click();
+      cy.wrap($tileList)
+        .eq(index - 1)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 1)
+        .click();
+      cy.wrap($tileList)
+        .eq(index - 2)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 2)
+        .click();
+    });
+    cy.getBoardStatus().should("have.text", "Winner: X");
+  });
+
+  it("shoud detect player O win", () => {
+    cy.getGameTileList().then(($tileList) => {
+      let index = 0;
+      cy.wrap($tileList).eq(index).click();
+      cy.wrap($tileList)
+        .eq(index - 1)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 1)
+        .click();
+      cy.wrap($tileList)
+        .eq(index - 2)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 2)
+        .click();
+    });
+    cy.getBoardStatus().should("have.text", "Winner: X");
+  });
+
   it("should detect top horizontal win ", () => {
     cy.getGameTileList().then(($tileList) => {
       let index = 0;
       cy.wrap($tileList).eq(index).click();
-      cy.wrap($tileList).eq(index - 1).click();
-      cy.wrap($tileList).eq(index + 1).click();
-      cy.wrap($tileList).eq(index - 2).click();
-      cy.wrap($tileList).eq(index + 2).click();
+      cy.wrap($tileList)
+        .eq(index - 1)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 1)
+        .click();
+      cy.wrap($tileList)
+        .eq(index - 2)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 2)
+        .click();
     });
     cy.getBoardStatus().should("have.text", "Winner: X");
   });
@@ -55,10 +96,18 @@ describe("Test case 3: Win conditions", () => {
     cy.getGameTileList().then(($tileList) => {
       let index = 3;
       cy.wrap($tileList).eq(index).click();
-      cy.wrap($tileList).eq(index - 1).click();
-      cy.wrap($tileList).eq(index + 1).click();
-      cy.wrap($tileList).eq(index - 2).click();
-      cy.wrap($tileList).eq(index + 2).click();
+      cy.wrap($tileList)
+        .eq(index - 1)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 1)
+        .click();
+      cy.wrap($tileList)
+        .eq(index - 2)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 2)
+        .click();
     });
     cy.getBoardStatus().should("have.text", "Winner: X");
   });
@@ -67,10 +116,18 @@ describe("Test case 3: Win conditions", () => {
     cy.getGameTileList().then(($tileList) => {
       let index = 6;
       cy.wrap($tileList).eq(index).click();
-      cy.wrap($tileList).eq(index - 1).click();
-      cy.wrap($tileList).eq(index + 1).click();
-      cy.wrap($tileList).eq(index - 2).click();
-      cy.wrap($tileList).eq(index + 2).click();
+      cy.wrap($tileList)
+        .eq(index - 1)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 1)
+        .click();
+      cy.wrap($tileList)
+        .eq(index - 2)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 2)
+        .click();
     });
     cy.getBoardStatus().should("have.text", "Winner: X");
   });
@@ -79,22 +136,38 @@ describe("Test case 3: Win conditions", () => {
     cy.getGameTileList().then(($tileList) => {
       let index = 0;
       cy.wrap($tileList).eq(index).click();
-      cy.wrap($tileList).eq(index + 1).click();
-      cy.wrap($tileList).eq(index + 3).click();
-      cy.wrap($tileList).eq(index + 2).click();
-      cy.wrap($tileList).eq(index + 6).click();
+      cy.wrap($tileList)
+        .eq(index + 1)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 3)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 2)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 6)
+        .click();
     });
     cy.getBoardStatus().should("have.text", "Winner: X");
   });
-  
+
   it("should detect middle vertical win", () => {
     cy.getGameTileList().then(($tileList) => {
       let index = 1;
       cy.wrap($tileList).eq(index).click();
-      cy.wrap($tileList).eq(index + 1).click();
-      cy.wrap($tileList).eq(index + 3).click();
-      cy.wrap($tileList).eq(index + 2).click();
-      cy.wrap($tileList).eq(index + 6).click();
+      cy.wrap($tileList)
+        .eq(index + 1)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 3)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 2)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 6)
+        .click();
     });
     cy.getBoardStatus().should("have.text", "Winner: X");
   });
@@ -103,10 +176,18 @@ describe("Test case 3: Win conditions", () => {
     cy.getGameTileList().then(($tileList) => {
       let index = 2;
       cy.wrap($tileList).eq(index).click();
-      cy.wrap($tileList).eq(index + 1).click();
-      cy.wrap($tileList).eq(index + 3).click();
-      cy.wrap($tileList).eq(index + 2).click();
-      cy.wrap($tileList).eq(index + 6).click();
+      cy.wrap($tileList)
+        .eq(index + 1)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 3)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 2)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 6)
+        .click();
     });
     cy.getBoardStatus().should("have.text", "Winner: X");
   });
@@ -115,10 +196,18 @@ describe("Test case 3: Win conditions", () => {
     cy.getGameTileList().then(($tileList) => {
       let index = 0;
       cy.wrap($tileList).eq(index).click();
-      cy.wrap($tileList).eq(index + 1).click();
-      cy.wrap($tileList).eq(index + 4).click();
-      cy.wrap($tileList).eq(index + 2).click();
-      cy.wrap($tileList).eq(index + 8).click();
+      cy.wrap($tileList)
+        .eq(index + 1)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 4)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 2)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 8)
+        .click();
     });
     cy.getBoardStatus().should("have.text", "Winner: X");
   });
@@ -127,12 +216,19 @@ describe("Test case 3: Win conditions", () => {
     cy.getGameTileList().then(($tileList) => {
       let index = 2;
       cy.wrap($tileList).eq(index).click();
-      cy.wrap($tileList).eq(index + 3).click();
-      cy.wrap($tileList).eq(index + 2).click();
-      cy.wrap($tileList).eq(index + 5).click();
-      cy.wrap($tileList).eq(index + 4 + 100).click();
+      cy.wrap($tileList)
+        .eq(index + 3)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 2)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 5)
+        .click();
+      cy.wrap($tileList)
+        .eq(index + 4)
+        .click();
     });
     cy.getBoardStatus().should("have.text", "Winner: X");
   });
 });
-
